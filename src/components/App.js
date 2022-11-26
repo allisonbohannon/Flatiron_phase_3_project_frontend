82e06852a77408d0588b38f447c313d9c2155d14
Allison Bohannon <akboh.github@gmail.com>
2022-11-26 14:24:58 -0500

    Wrap App.js render in router, create route paths

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,29 @@
 import React, {useState, useEffect} from "react";
-import { Route, Routes} from "react-router-dom";
+import { Route, Routes} from "react-router";
+import  NavigationBar  from "./NavigationBar"
+import ByAuthor from "./ByAuthor"
+import ByGenre from "./ByGenre"
+import CompletedReads from "./CompletedReads"
+import Home from "./Home"
 
 function App() {
 
-    return 'Hello World'
+    return (
+        <div>
+            'Hello,world'
+            <NavigationBar/>
+            <Routes>
+                <Route path="/search_by_author" element={<ByAuthor 
+                />} />
+                <Route path="/search_by_genre" element={<ByGenre 
+                />} />
+                <Route path="/completed_reads" element={<CompletedReads 
+                />} />
+                <Route path="/" element={<Home
+                />} />
+            </Routes>
+        </div>
+    )
 
 }
 
