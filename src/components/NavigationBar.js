644ad59a710ddbcaab03addb9a7ed5e8d64a7a3f
Allison Bohannon <akboh.github@gmail.com>
2022-11-26 14:29:50 -0500

    Build NavigationBar

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,8 +1,50 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 const NavigationBar = () => {
   return (
-    <div>NavigationBar</div>
+    <Navbar  bg="light">
+    <Nav defaultActiveKey="home">
+      <Nav.Item>
+        <Nav.Link 
+          as={Link}
+          to="/"
+          eventKey="home"
+        >
+          Home
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link
+          as={Link}
+          to="/search_by_author"
+          eventKey="search_by_author"
+        >
+          Search By Author
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link
+          as={Link}
+          to="/search_by_genre"
+          eventKey="search_by_genre"
+        >
+          Seach by Genre
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link
+          as={Link}
+          to="/completed_reads"
+          eventKey="completed_reads"
+        >
+          Completed Reads
+        </Nav.Link>
+      </Nav.Item>
+  </Nav>
+</Navbar>
   )
 }
 
