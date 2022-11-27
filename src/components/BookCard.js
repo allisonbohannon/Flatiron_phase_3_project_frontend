c190de068761d5bb608f7b0e4c21a2804a3d1c9a
Allison Bohannon <akboh.github@gmail.com>
2022-11-26 21:42:09 -0500

    Add bootstrap styling

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -1,16 +1,19 @@
 import React from 'react'
+import Card from 'react-bootstrap/Card';
 
 const BookCard = ({book}) => {
 
   
   
   return (
-    <div>
-        {book.title}
-        {book.author.first_name}
-        {book.author.last_name}
-        {book.genre.genre}
-    </div>
+    <Card  style={{ margin: '2rem', padding: '1em', width:"24rem"}}
+    cols={2}>
+      <Card.Body>
+        <Card.Title> {book.title}</Card.Title>
+        <Card.Text>{`By: ${book.author.first_name} ${book.author.last_name} `}</Card.Text>
+        <Card.Subtitle>{book.genre.genre}</Card.Subtitle>
+      </Card.Body>  
+    </Card>
   )
 }
 
